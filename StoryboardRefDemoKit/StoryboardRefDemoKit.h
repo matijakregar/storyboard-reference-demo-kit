6db1ae4eddcab30c588ec4dc13542e6effc894d8
Matija Kregar <matija.kregar@gmail.com>
2016-11-03 11:56:46 +0100

    Initial Commit

---
+++ StoryboardRefDemoKit/StoryboardRefDemoKit.h
@@ -0,0 +1,19 @@
+//
+//  StoryboardRefDemoKit.h
+//  StoryboardRefDemoKit
+//
+//  Created by Matija Kregar on 03/11/2016.
+//  Copyright © 2016 Matija Kregar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StoryboardRefDemoKit.
+FOUNDATION_EXPORT double StoryboardRefDemoKitVersionNumber;
+
+//! Project version string for StoryboardRefDemoKit.
+FOUNDATION_EXPORT const unsigned char StoryboardRefDemoKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StoryboardRefDemoKit/PublicHeader.h>
+
+
